00c4895cdb70f3442d6fa14da1522fbb3dfefa97
BrentSoler <brntsoler@gmail.com>
2022-10-27 20:59:02 +0800

    feat:added userstable and activating users

--- src/pages/MainDashboard.jsx
+++ src/pages/MainDashboard.jsx
@@ -1,10 +1,14 @@
 import Navbar from "../components/Dashboard/Navbar";
+import UserTable from "../components/userTable/userTable";
 
 const Dashboard = () => {
 	return (
-	<>
-	<Navbar />
-	</>
+		<div>
+			<Navbar />
+			<div className="min-h-[90vh] flex justify-center p-8">
+				<UserTable />
+			</div>
+		</div>
 	);
 };
 
